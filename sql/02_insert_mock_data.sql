a88d3856ea662eed261d47e2026d41c872ba7ceb
Maria Paz Battistoni <battistonimpaz@gmail.com>
2026-08-28 08:02:36 -0300

    Agrego script de mock data con ingredientes y receta inicial

--- sql/02_insert_mock_data.sql
+++ sql/02_insert_mock_data.sql
@@ -1,1 +1,13 @@
--- Script de carga de datos 
+-- Script de carga de datos
+INSERT INTO ingredients (name, calories_per_100g, protein_g, carbs_g, fat_g) VALUES
+('Avena en hojuelas', 389, 16.9, 66.3, 6.9),
+('Mantequilla de maní', 588, 25.0, 20.0, 50.0),
+('Semillas de chía', 486, 16.5, 42.1, 30.7);
+
+INSERT INTO recipes (name, description) VALUES
+('Barritas de Granola Fit', 'Snack rápido y alto en fibra, sin cocción.');
+
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity_g) VALUES
+(1, 1, 200.00), -- Relaciona con la Avena
+(1, 2, 100.00), -- Relaciona con la Mantequilla de maní
+(1, 3, 30.00);  -- Relaciona con las Semillas de chía
